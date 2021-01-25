5672363c9ede040e2b8329c67647cfd7f5283bfb
Evgprog <konfuzy@gmail.com>
2021-01-25 22:28:38 +0200

    Initial Commit

---
+++ City_row_model.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+class city_row_model:
+    def __init__(self, data, regression_model, city_name,score):
+        self.data = data
+        self.model = regression_model
+        self.name = city_name[::-1]
+        self.score= score
+
+    def predict(self, regressors):
+        predictions = self.model.predict(regressors[:, np.newaxis])
+        return predictions
+
+    def show_regression(self, regressors):
+        plt.figure(figsize=(8,7))
+        ax = plt.axes()
+        ax.title(self.name)
+        ax.scatter(regressors, self.data)
+
+        x_new = np.linspace(0, 30, 100)
+        y_new = self.model.predict(x_new[:, np.newaxis])
+        ax.plot(x_new, y_new)
+        ax.axis('tight')
+        plt.show(block=True)
