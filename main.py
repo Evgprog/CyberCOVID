a7f2bdd1e3042be932bb3c6a8afb1ed0f7599c5c
Evgprog <konfuzy@gmail.com>
2021-02-11 00:52:11 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -4,25 +4,10 @@ Created on Mon Jan 11 23:40:00 2021
 
 @author: evgeny
 """
-# Project's aims: To allow localizaton of quarantine policy measures due to early identification of covid spread
-# Currently a more binary policy is being used where all or no cities are quarantied
-# the measures for infectios deseace prevention were used on the number o admitted patients
-# risk analysis is done on overall number of patients which were identified
-# and earlier detection based on the  prediction of possible number off patients might allow a more proactive measure to take place
 # The structure of analysis :
 # Getting data regarding number of patients by city
 # Normalizing data, fixing mistakes
 # Creating predictions for spreading rate per city
-# Using formula identified in the article x35
-# Read me for GITHUB
-# The code performs predictions of potential spread of covid 19 in Israil's cities
-# The codes's structure consist of the following parts:
-# 1. Data upload from a website that contains a history of patients dinamycs  per city
-# 2. Data  insertion into a pandas dataframework
-# 3. Cleaning and prepearing of dataframe for analysis
-# 4. Creating several models and classes for linear regression analysis and predictions
-# 5. Creating a trashold for data set on 08 score
-# 5. Applying models for each city dataset
 
 import io
 import numpy as np
